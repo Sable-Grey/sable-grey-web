14ac4869a6f4d75b510b6c1e12ff7d8668f206de
Romeo <romeomeshack34@gmail.com>
2026-05-23 09:11:20 +0100

    modified

--- out/images/slider/slider-entry.ts
+++
@@ -1,6 +0,0 @@
-import image1 from "./image-1.avif";
-import image2 from "./image-2.avif";
-import image3 from "./image-3.avif";
-import image4 from "./image-4.avif";
-
-export default [image1, image2, image3, image4];
